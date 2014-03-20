f1e6826d33d2fdfd89d3a14f5ecc01c4294271f1
Corbin Page <corbin.page@gmail.com>
2014-03-20 18:56:34 -0400

    DEPRECATING THIS PROJECT. SEE DIRTY LAUNDRY

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,29 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  private
+  def login_required
+    if !logged_in?
+      redirect_to new_user_session_path, :notice => "Login required!"
+    end
+  end
+
+  def login(user)
+    session[:user_id] = user.id
+  end
+
+  def logout
+    reset_session
+  end
+  
+  def logged_in?
+    !!current_user
+  end
+  helper_method :logged_in?
+
+  def current_user
+    @current_user ||= User.find_by(:id => session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
+
 end
